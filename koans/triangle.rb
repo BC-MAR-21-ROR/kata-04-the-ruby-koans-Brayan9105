98a51d4e7d8f8aeb265d4fb608801ff88fb24aea
Brayan Lopez <b.lopez@opensky.com.co>
2021-03-31 20:22:47 -0500

    Add koans solution from 125 to 170

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,18 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+  [:equilateral,:isosceles,:scalene].fetch([a,b,c].uniq.size - 1)
+
+  if a == b && a == c
+    :equilateral
+  elsif a == b || a == c || b == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
